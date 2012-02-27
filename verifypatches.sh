7f725b71a7e2ebd42bf20bc381a6b5c02ad164a7
Aaron Russo <arusso@berkeley.edu>
2012-02-27 13:23:28 -0800

    added httpd service
    cleaned up code a bit, switched to storing processes/services in arrays so we can print them together

--- verifypatches.sh
+++ verifypatches.sh
@@ -11,38 +11,31 @@ function get_service_name() {
     SERVICE=""
     case $1 in
 	acpid) # acpi daemon
-	    SERVICE=acpid
-	    ;;
+	    SERVICE=acpid ;;
+	atd) # at daemon
+	    SERVICE=atd ;;
 	auditd) # audit daemon
-	    SERVICE=auditd
-	    ;;
+	    SERVICE=auditd ;;
 	cfexecd) # cfengine agent daemon
-	    SERVICE=cfexecd
-	    ;;
+	    SERVICE=cfexecd ;;
 	conntrack) # conntrack logging daemon
-	    SERVICE=conntrack-log
-	    ;;
+	    SERVICE=conntrack-log ;;
 	crond) # cron daemon
-	    SERVICE=crond
-	    ;;
+	    SERVICE=crond ;;
+	httpd)
+	    SERVICE=httpd ;;
 	iscsid) # iscsi daemon
-	    SERVICE=iscsid
-	    ;;
+	    SERVICE=iscsid ;;
 	ntpd) 
-	    SERVICE=ntpd
-	    ;;
+	    SERVICE=ntpd ;;
 	rsyslogd)
-	    SERVICE=rsyslogd
-	    ;;
+	    SERVICE=rsyslogd ;;
 	snmpd)
-	    SERVICE=snmpd
-	    ;;
+	    SERVICE=snmpd ;;
 	'ssh'|'sshd')
-	    SERVICE=openssh-daemon
-	    ;;
+	    SERVICE=openssh-daemon ;;
 	xinetd)
-	    SERVICE=xinetd
-	    ;;
+	    SERVICE=xinetd ;;
     esac
 }
 
@@ -69,29 +62,52 @@ echo LSOF_VER=$LSOF_VER
 
 # based on lsof version, get a list of all the process names that have old
 # libraries loaded
-OLD_LIBS=""
 case "$LSOF_VER" in
     "4.78") # RHEL5
-	OLD_LIBS="`lsof -T | grep inode= | cut -d ' ' -f 1 | sort -u`"
+	OLD_LIBS=(`lsof -T | grep inode= | cut -d ' ' -f 1 | sort -u`)
 	;;
     *) # default
 	echo "This version of lsof has not been accounted for."
 	exit $ERR_BAD_LSOF_VER
 esac
 
-
 # check that we have processes to make recommendations on
-if [ "$OLD_LIBS" == "" ]; then
-    echo "Everything looks in order here...";
-    exit 0;
-fi
+#if [ "$OLD_LIBS" == "" ]; then
+#    echo "Everything looks in order here...";
+#    exit 0;
+#fi
 
 # Now lets iterate through our list and recommend a service to restart
-echo "$OLD_LIBS" | while read PROCESS; do
+declare -a RESTART_SERVER
+declare -a RESTART_SERVICE
+for PROCESS in ${OLD_LIBS[@]}
+  do
     get_service_name $PROCESS
     if [ "$SERVICE" == "" ]; then
-	echo No service associated with process \($PROCESS\)... Restart suggested
+	RESTART_SERVER=( "${RESTART_SERVER[@]}" "$PROCESS" )
     else
-	echo service restart $SERVICE
+	RESTART_SERVICE=( "${RESTART_SERVICE[@]}" "$SERVICE" )
     fi
 done
+
+#for element in ${RESTART_SERVICE[@]}
+#  do
+#  echo $element
+#done
+
+
+if [ ${#RESTART_SERVER[0]} -gt 0 ]; then
+    echo "*** The following processes have no know affiliated service, and require a restart (or other undefinied intervention) to reload"
+    for PROCESS in ${RESTART_SERVER[@]}
+      do
+      echo $'\t'"$PROCESS"
+    done
+fi
+
+if [ ${#RESTART_SERVICE[0]} -gt 0 ]; then
+    echo "*** The following services can be restarted"
+    for SERVICE in ${RESTART_SERVICE[@]}
+      do
+      echo $'\t'"service $SERVICE restart"
+    done
+fi
